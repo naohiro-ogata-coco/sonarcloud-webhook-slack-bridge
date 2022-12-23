26a78c9dd465cb78adf69f624501382e7368d0c1
Naohiro Oogata <oogatta@gmail.com>
2022-12-24 00:01:28 +0900

    環境変数でSlackWebhookUrlをセットできるようにする

--- README.md
+++ README.md
@@ -39,6 +39,7 @@ To build and deploy your application for the first time, run the following in yo
 ```bash
 sam build
 sam deploy --guided
+ sam deploy --parameter-overrides SlackWebhookUrl=https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX
 ```
 
 The first command will build the source of your application. The second command will package and deploy your application to AWS, with a series of prompts:
@@ -66,7 +67,7 @@ Test a single function by invoking it directly with a test event. An event is a 
 Run functions locally and invoke them with the `sam local invoke` command.
 
 ```bash
-sonarcloud-webhook-slack-bridge$ npx ts-node util/event.ts | sam local invoke SonarCloudWebhookSlackBridgeFunction --event -
+sonarcloud-webhook-slack-bridge$ npx ts-node util/event.ts | sam local invoke SonarCloudWebhookSlackBridgeFunction --event - --parameter-overrides SlackWebhookUrl=https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXX
 ```
 
 The SAM CLI can also emulate your application's API. Use the `sam local start-api` to run the API locally on port 3000.
--- sonarcloud-webhook-slack-bridge/app.ts
+++ sonarcloud-webhook-slack-bridge/app.ts
@@ -1,4 +1,5 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { IncomingWebhook } from '@slack/webhook';
 
 /**
  *
@@ -12,6 +13,14 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        if (!event.body) {
+            // noinspection ExceptionCaughtLocallyJS
+            throw new Error('No body in event');
+        }
+        const sonarWebhookBody = JSON.parse(event.body);
+
+        await sendSlakWebhook(sonarWebhookBody.status);
+
         return {
             statusCode: 200,
             body: JSON.stringify({
@@ -28,3 +37,15 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         };
     }
 };
+
+async function sendSlakWebhook(status: string) {
+    const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+    if (!slackWebhookUrl) {
+        // noinspection ExceptionCaughtLocallyJS
+        throw new Error('SLACK_WEBHOOK_URL is not set');
+    }
+    const webhook = new IncomingWebhook(slackWebhookUrl);
+    await webhook.send({
+        text: status,
+    });
+}
